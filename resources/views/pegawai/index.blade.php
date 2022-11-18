d08eaafef3f4767e9fa4a1975c70b13a0a6abb7d
12GitHubGami <gamiatsugani0990@gmail.com>
2022-11-18 09:18:50 +0700

    first commit

---
+++ resources/views/pegawai/index.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.index')
+@section('content')
+<div class="col-12">
+    <div class="card recent-sales overflow-auto">
+        <div class="card-body">
+            <h5 class="card-title">Data Pegawai</h5>
+
+            <table class="table table-borderless datatable">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">NIP</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Jabatan</th>
+                        <th scope="col">Divisi</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php $no= 1; @endphp
+                    @foreach($pegawai as $row)
+                    <tr>
+                        <th scope="row">{{ $no++ }}</th>
+                        <td>{{ $row->nip }}</td>
+                        <td>{{ $row->nama }}</td>
+                        <td>{{ $row->jabatan->nama }}</td>
+                        <td>{{ $row->divisi->nama }}</td>
+                        <td>{{ $row->gender }}</td>
+                        <td>{{ $row->foto }}</td>
+                        <td>
+                            <a class="btn btn-info btn-sm" title="Detail Pegawai"
+                                href=" {{ route('pegawai.show',$row->id) }}">
+                                <i class="bi bi-eye"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+</div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\DivisiController;
+use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\PegawaiController;
+ 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/welcome', function () {
+    return '<h1>Semangat Belajar Laravel Framework</h1>';
+});
+
+Route::get('/salam', function () {
+    return view('halaman_salam');
+});
+
+Route::get('/pegawai/{nama}/{divisi}', function ($nama,$divisi) {
+    return '<ul>
+                <li>Nama: '.$nama.'</li>
+                <li>Divisi: '.$divisi.'</li>
+            </ul>';
+});
+
+Route::get('/nilai', function () {
+    return view('nilai');
+});
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'dataMahasiswa']);
+Route::get('/nilaimahasiswa', [MahasiswaController::class, 'nilaiMahasiswa']);
+
+//---------------routing landing page--------------
+Route::get('/', function () {
+    return view('landingpage.home');
+});
+
+Route::get('/home', function () {
+    return view('landingpage.home');
+});
+
+Route::get('/about', function () {
+    return view('landingpage.about');
+});
+
+Route::get('/contact', function () {
+    return view('landingpage.kontak');
+});
+
+Route::get('/login', function () {
+    return view('landingpage.login_form');
+});
+
+//---------------routing admin page--------------
+Route::get('/administrator', function () {
+    return view('admin.home');
+});
+
+Route::resource('staff',StaffController::class);
+Route::resource('divisi',DivisiController::class);
+Route::resource('jabatan',JabatanController::class);
+Route::resource('pegawai',PegawaiController::class);
